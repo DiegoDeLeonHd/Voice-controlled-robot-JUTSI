97bd161b5bb293ff309347fd9eb758e64fc0b5d6
Diego De Leon Hernandez <deleon.diego.hdz@gmail.com>
2025-05-29 14:01:04 -0600

    .

--- Nodes/Vision_node.py
+++
@@ -1,9 +0,0 @@
-print("hello jutsi the best proyect")
-print("sassa yacusa")
-
-print("dolaor")
-
-print("vivian los negros color blanquito")
-print("fuenen a Eunice")
-hola ya vamonos
-hoy si ya sale el proyecto 
